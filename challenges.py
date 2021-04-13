b0a14d6990f3f730fe8895a5bcf501f170d0a572
Daniel Gamboa <danielgamboa10@gmail.com>
2021-04-13 15:16:25 -0500

    Add problem 2.2.2

--- challenges.py
+++ challenges.py
@@ -576,3 +576,39 @@ def csFindTheSingleNumber(nums):
     
     sorted_store = sorted(store.items(), key=lambda e: e[1])
     return sorted_store[0][0]
+
+
+# ********PROBLEM 2***************************************************************************** #
+# Given a list of different students' scores, write a function that returns the average of each student's top five scores. You should return the averages in ascending order of the students' id numbers.
+
+# Each entry (scores[i]) has the student's id number (scores[i][0]) and the student's score (scores[i][1]). The averages should be calculated using integer division.
+
+# Example 1:
+
+# Input: [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
+# Output: [[1,87],[2,88]]
+# Explanation:
+# The average student `1` is `87`.
+# The average of student `2` is `88.6`, but with integer division is `88`.
+# Notes:
+
+# The score of the students is between 1 and 100.
+
+# Initial Submission
+def csAverageOfTopFive(scores):
+    store = {}
+    
+    for e in scores:
+        id = e[0]
+        if id not in store: store[id] = []
+        store[id].append(e[1])
+    
+    output = []
+    
+    for item in store.items():
+        id = item[0]
+        scores = sorted(item[1], reverse=True)
+        avg = sum(scores[:5]) // min(len(scores), 5)
+        output.append([id, avg])
+    
+    return output
