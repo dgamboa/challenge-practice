4de5fe67012fd1724ece6025ce506b5b70acf31c
Daniel Gamboa <danielgamboa10@gmail.com>
2021-04-20 10:50:24 -0500

    Add 3.2.2 parentheses check with stacks

--- challenges.py
+++ challenges.py
@@ -1012,3 +1012,33 @@ def queueOnStacks(requests):
             ans.append(remove())
     return ans
 
+
+
+# ********PROBLEM 1*********************************************************** #
+# Given a string sequence consisting of the characters '(', ')', '[', ']', '{', and '}'. Your task is to determine whether or not the sequence is a valid bracket sequence.
+
+# The Valid bracket sequence is defined in the following way:
+
+# An empty bracket sequence is a valid bracket sequence.
+# If S is a valid bracket sequence then (S), [S] and {S} are also valid.
+# If A and B are valid bracket sequences then AB is also valid.
+# Example
+
+# For sequence = "()", the output should be validBracketSequence(sequence) = true;
+# For sequence = "()[]{}", the output should be validBracketSequence(sequence) = true;
+# For sequence = "(]", the output should be validBracketSequence(sequence) = false;
+# For sequence = "([)]", the output should be validBracketSequence(sequence) = false;
+# For sequence = "{[]}", the output should be validBracketSequence(sequence) = true.
+def validBracketSequence(sequence):
+    stack = []
+    reference = {")": "(", "}": "{", "]": "["}
+    
+    for b in sequence:
+        if len(stack) == 0:
+            stack.append(b)
+        elif b in reference.keys() and stack[-1] == reference[b]:
+            stack.pop()
+        else:
+            stack.append(b)
+    
+    return True if len(stack) == 0 else False
