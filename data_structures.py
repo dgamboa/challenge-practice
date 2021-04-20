8d03373d07fbe0d7ce6e39e348121cc8490a5a83
Daniel Gamboa <danielgamboa10@gmail.com>
2021-04-20 09:31:15 -0500

    Add linked list, stack, queue

---
+++ data_structures.py
@@ -0,0 +1,120 @@
+# **************************************************************************** #
+# Linked List
+# **************************************************************************** #
+class LinkedListNode:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self, head=None):  
+        self.head = head
+
+    def append(self, data):
+        new_node = LinkedListNode(data)
+
+        if self.head:
+            current = self.head
+
+            while current.next:
+                current = current.next
+
+            current.next = new_node
+         else:
+             self.head = new_node
+
+# Sample use of the linked list
+# >>> a = LinkedListNode(1)
+# >>> my_ll = LinkedList(a)
+# >>> my_ll.append(2)
+# >>> my_ll.append(3)
+# >>> my_ll.head.data
+# 1
+# >>> my_ll.head.next.data
+# 2
+# >>> my_ll.head.next.next.data
+# 3
+# >>>
+
+# **************************************************************************** #
+# Queue using a singly-linked list
+# **************************************************************************** #
+class LinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+    def enqueue(self, item):
+        new_node = LinkedListNode(item)
+        # check if queue is empty
+        if self.rear is None:
+            self.front = new_node
+            self.rear = new_node
+        else:
+            # add new node to rear
+            self.rear.next = new_node
+            # reassign rear to new node
+            self.rear = new_node
+    def dequeue(self):
+        # check if queue is empty
+        if self.front is not None:
+            # keep copy of old front
+            old_front = self.front
+            # set new front
+            self.front = old_front.next
+
+        # check if the queue is now empty
+        if self.front is None:
+            # make sure rear is also None
+            self.rear = None
+
+        return old_front
+
+# **************************************************************************** #
+# Stack with a dynamic array
+# **************************************************************************** #
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        if len(self.data) > 0:
+            return self.data.pop()
+        return "The stack is empty"
+
+# **************************************************************************** #
+# Stack with a linked list
+# **************************************************************************** #
+class LinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        # create new node with data
+        new_node = LinkedListNode(data)
+        # set current top to new node's next
+        new_node.next = self.top
+        # reset the top pointer to the new node
+        self.top = new_node
+
+    def pop(self):
+        # make sure stack is not empty
+        if self.top is not None:
+            # store popped node
+            popped_node = self.top
+            # reset top pointer to next node
+            self.top = popped_node.next
+            # return the value from the popped node
+            return popped_node.data
