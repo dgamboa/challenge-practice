2d313655155c7f2ac2278262279e9df08645b52d
Daniel Gamboa <danielgamboa10@gmail.com>
2021-04-22 15:48:16 -0500

    Add 3.4.2 binary tree breadth first

--- challenges.py
+++ challenges.py
@@ -1172,3 +1172,69 @@ def binaryTreeInOrderTraversal(root):
     result = []
     search(root, result)
     return result
+
+# ********PROBLEM 2*********************************************************** #
+# Note: Try to solve this task without using recursion, since this is what you'll be asked to do during an interview.
+
+# Given a binary tree of integers t, return its node values in the following format:
+
+# The first element should be the value of the tree root;
+# The next elements should be the values of the nodes at height 1 (i.e. the root children), ordered from the leftmost to the rightmost one;
+# The elements after that should be the values of the nodes at height 2 (i.e. the children of the nodes at height 1) ordered in the same way;
+# Etc.
+# Example
+
+# For
+
+# t = {
+#     "value": 1,
+#     "left": {
+#         "value": 2,
+#         "left": null,
+#         "right": {
+#             "value": 3,
+#             "left": null,
+#             "right": null
+#         }
+#     },
+#     "right": {
+#         "value": 4,
+#         "left": {
+#             "value": 5,
+#             "left": null,
+#             "right": null
+#         },
+#         "right": null
+#     }
+# }
+# the output should be
+# traverseTree(t) = [1, 2, 4, 3, 5].
+
+# Initial Submission:
+from collections import deque
+#
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+def traverseTree(t):
+    if t is None:
+        return []
+    
+    result = []
+    nodes = deque()
+    nodes.append(t)
+    
+    while len(nodes) > 0:
+        n = nodes.popleft()
+            
+        if n is not None:
+            nodes.append(n.left)
+            nodes.append(n.right)
+            result.append(n.value)
+    
+    return result
+            
+
