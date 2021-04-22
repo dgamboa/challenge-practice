acd44b34b3b321d5ef7cdf3f3f4d2a2901a5893e
Daniel Gamboa <danielgamboa10@gmail.com>
2021-04-22 17:35:09 -0500

    Add 3.4.3 formatted binary tree output

--- challenges.py
+++ challenges.py
@@ -1238,3 +1238,69 @@ def traverseTree(t):
     return result
             
 
+# ********PROBLEM 3*********************************************************** #
+# Given a binary tree of integers, return all the paths from the tree's root to its leaves as an array of strings. The strings should have the following format:
+# "root->node1->node2->...->noden", representing the path from root to noden, where root is the value stored in the root and node1,node2,...,noden are the values stored in the 1st, 2nd,..., and nth nodes in the path respectively (noden representing the leaf).
+
+# Example
+
+# For
+
+# t = {
+#     "value": 5,
+#     "left": {
+#         "value": 2,
+#         "left": {
+#             "value": 10,
+#             "left": null,
+#             "right": null
+#         },
+#         "right": {
+#             "value": 4,
+#             "left": null,
+#             "right": null
+#         }
+#     },
+#     "right": {
+#         "value": -3,
+#         "left": null,
+#         "right": null
+#     }
+# }
+# the output should be
+# treePaths(t) = ["5->2->10", "5->2->4", "5->-3"].
+
+# The given tree looks like this:
+
+#     5
+#    / \
+#   2  -3
+#  / \
+# 10  4
+
+# Initial Submission:
+#
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+def search(root, res, tracker):
+    if root is None:
+        return
+    
+    res.append(root.value)
+    search(root.left, res, tracker)
+    search(root.right, res, tracker)
+    
+    if root.left is None and root.right is None:
+        tracker.append("->".join([str(i) for i in res]))
+    
+    res.pop()
+
+def treePaths(t):
+    res = []
+    tracker = []
+    search(t, res, tracker)
+    return tracker
