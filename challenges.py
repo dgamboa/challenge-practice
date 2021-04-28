230b94a967a7ffd29a9ea04104076ecdd5775a82
Daniel Gamboa <danielgamboa10@gmail.com>
2021-04-28 12:06:30 -0500

    Add problem 3 final sprint challenge

--- challenges.py
+++ challenges.py
@@ -1675,3 +1675,45 @@ def first_not_repeating_character(s):
     return "_" if len(chars) == 0 else chars[0]
 
 
+# ********PROBLEM 3*********************************************************** #
+# In a city-state of n people, there is a rumor going around that one of the n people is a spy for the neighboring city-state.
+
+# The spy, if it exists:
+
+# Does not trust anyone else.
+# Is trusted by everyone else (he's good at his job).
+# Works alone; there are no other spies in the city-state.
+# You are given a list of pairs, trust. Each trust[i] = [a, b] represents the fact that person a trusts person b.
+
+# If the spy exists and can be found, return their identifier. Otherwise, return -1.
+
+# Example 1:
+
+# Input: n = 2, trust = [[1, 2]]
+# Output: 2
+# Explanation: Person 1 trusts Person 2, but Person 2 does not trust Person 1, so Person 2 is the spy.
+# Example 2:
+
+# Input: n = 3, trust = [[1, 3], [2, 3]]
+# Output: 3
+# Explanation: Person 1 trusts Person 3, and Person 2 trusts Person 3, but Person 3 does not trust either Person 1 or Person 2. Thus, Person 3 is the spy.
+def uncover_spy(n, trust):
+    t = {}
+    
+    for i in range(1, n+1):
+        t[i] = []
+    
+    for truster, trusted in trust:
+        t[truster].append(trusted)
+    
+    for i in range(1, n+1):
+        if t[i] == []:
+            count = 0
+            for j in range(1, n+1):
+                if i in t[j]:
+                    count += 1
+            
+            if count == n - 1:
+                return i
+    
+    return -1
