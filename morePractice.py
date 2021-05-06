bc98bc397661ea7e7f213061e8aca49819e59fa1
Daniel Gamboa <danielgamboa10@gmail.com>
2021-05-05 19:42:49 -0500

    Add HTML end tag

--- morePractice.py
+++ morePractice.py
@@ -41,3 +41,24 @@ def properNounCorrection(noun):
 # isTandemRepeat(inputString) = false.
 def isTandemRepeat(inputString):
     return inputString[len(inputString)//2:] == inputString[:len(inputString)//2]
+
+# **************************************************************************** #
+# HTML End Tag by Start Tag
+# **************************************************************************** #
+# You are implementing your own HTML editor. To make it more comfortable for developers you would like to add an auto-completion feature to it.
+
+# Given the starting HTML tag, find the appropriate end tag which your editor should propose.
+
+# If you are not familiar with HTML, consult with this note.
+
+# Example
+
+# For startTag = "<button type='button' disabled>", the output should be
+# htmlEndTagByStartTag(startTag) = "</button>";
+# For startTag = "<i>", the output should be
+# htmlEndTagByStartTag(startTag) = "</i>".
+def htmlEndTagByStartTag(startTag):
+    i = startTag.find(' ')
+    front = startTag[0] + "/"
+    
+    return front + startTag[1:i] + ">" if i + 1 else front + startTag[1:]
