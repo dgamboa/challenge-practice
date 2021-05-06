8fbfc3e8330aafb5cbaaa047ca0eeaed1793d4ed
Daniel Gamboa <danielgamboa10@gmail.com>
2021-05-05 19:31:14 -0500

    Add proper noun correction

--- morePractice.py
+++ morePractice.py
@@ -13,3 +13,15 @@ def encloseInBrackets(inputString):
 # **************************************************************************** #
 # Proper Noun Correction
 # **************************************************************************** #
+# Proper nouns always begin with a capital letter, followed by small letters.
+
+# Correct a given proper noun so that it fits this statement.
+
+# Example
+
+# For noun = "pARiS", the output should be
+# properNounCorrection(noun) = "Paris";
+# For noun = "John", the output should be
+# properNounCorrection(noun) = "John".
+def properNounCorrection(noun):
+    return noun[0].capitalize() + noun[1:].casefold()
