a6a860619636aaacc8fcbe02519c1dbaf0a28f26
Daniel Gamboa <danielgamboa10@gmail.com>
2021-06-08 14:05:51 -0500

    Binary search where no find returns appropriate index

--- leetcode.py
+++ leetcode.py
@@ -255,3 +255,30 @@ class Solution:
             ans.append(y)
         
         return ans
+
+
+# **************************************************************************** #
+# *********** Exercise ******************************************************* #
+# **************************************************************************** #
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+# (i.e. binary search algorithm that returns the index if it finds the number,
+# or the index where the number should go if it doesn't find it)
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:      
+        start = 0
+        end = len(nums) - 1
+        
+        while start <= end:
+            mid = (start + end) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+            
+        return start
