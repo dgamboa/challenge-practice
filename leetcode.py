fbaacf6458a20633d54397005aa7d977619bf254
Daniel Gamboa <danielgamboa10@gmail.com>
2021-06-08 15:01:25 -0500

    Solve two sums in O(n) with a hash table

--- leetcode.py
+++ leetcode.py
@@ -282,3 +282,23 @@ class Solution:
                 end = mid - 1
             
         return start
+
+
+# **************************************************************************** #
+# *********** Exercise ******************************************************* #
+# **************************************************************************** #
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, n in enumerate(nums):
+            if target - n in seen:
+                return [seen[target - n], i]
+            else:
+                seen[n] = i
+        
+        return "No solution"
