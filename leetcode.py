4b3302c93ba19bf5649b25a71fb370e6a547e76d
Daniel Gamboa <danielgamboa10@gmail.com>
2021-06-10 11:05:11 -0500

    sumZero problem

--- leetcode.py
+++ leetcode.py
@@ -302,3 +302,23 @@ class Solution:
                 seen[n] = i
         
         return "No solution"
+
+# **************************************************************************** #
+# *********** Exercise ******************************************************* #
+# **************************************************************************** #
+# Given an integer n, return any array containing n unique integers such that they add up to 0.
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        ans = []
+        
+        if n % 2 == 0:
+            for i in range(1, n//2 + 1):
+                ans.append(i)
+                ans.append(-i)
+        else:
+            for i in range(1, n//2 + 1):
+                ans.append(i)
+                ans.append(-i)
+            ans.append(0)
+        
+        return ans
