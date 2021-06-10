ca449d331031c81947a2a2349a7e447671f98e2f
Daniel Gamboa <danielgamboa10@gmail.com>
2021-06-10 11:08:06 -0500

    Refactor the sumZero problem solution

--- leetcode.py
+++ leetcode.py
@@ -322,3 +322,17 @@ class Solution:
             ans.append(0)
         
         return ans
+
+# Refactor
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        ans = []
+        
+        for i in range(1, n//2 + 1):
+                ans.append(i)
+                ans.append(-i)
+        
+        if n % 2 != 0:
+            ans.append(0)
+        
+        return ans
