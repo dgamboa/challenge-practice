b410be7baa794b4d032a2816ef211b2eac2bf496
Daniel Gamboa <danielgamboa10@gmail.com>
2021-06-12 13:55:46 -0500

    Add exercise 2 on sorting characters

--- Byte/strings_review.py
+++ Byte/strings_review.py
@@ -29,4 +29,43 @@ class Solution:
 # Runtime: 48 ms, faster than 68.28% of Python3 online submissions for Valid Anagram.
 # Memory Usage: 14.4 MB, less than 76.38% of Python3 online submissions for Valid Anagram.
 
+# Solution using get dict method (would work with any encoding system):
+def isAnagramGet(self, s, t):
+    dic1, dic2 = {}, {}
+    for item in s:
+        dic1[item] = dic1.get(item, 0) + 1
+    for item in t:
+        dic2[item] = dic2.get(item, 0) + 1
+    return dic1 == dic2
+
+# Solution using fixed-length array sort (would not work with extended characters):
+def isAnagramFixedArraySort(self, s, t):
+    s_d, t_d = [0]*26, [0]*26
+    for item in s:
+        s_d[ord(item)-ord('a')] += 1
+    for item in t:
+        t_d[ord(item)-ord('a')] += 1
+    return s_d == t_d
+
+# Trivial sort solution (time complexity is higher at O(n*logn)):
+def isAnagramTrivialSort(self, s, t):
+    return sorted(s) == sorted(t)
+
 # Exercise 2: Sorting the characters in a string ( https://www.geeksforgeeks.org/sort-string-characters/ )
+def sortString(s):
+      store = [0]*26
+
+      for c in s:
+        store[ord(c) - ord('a')] += 1
+      
+      ans = ''
+      for i, count in enumerate(store):
+        ans += chr(i + ord('a')) * count
+      
+      return ans
+
+# Trivial sort solution (time complexity is higher at O(n*logn)):
+def sortStringTrivial(s):
+  return ''.join(sorted(s))
+
+# Exercise 3: Longest substring without repeating characters (https://leetcode.com/problems/longest-substring-without-repeating-characters/)
