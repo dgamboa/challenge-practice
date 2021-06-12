a9d9cbe7ac4059792275a55d32d93d02be0e13d3
Daniel Gamboa <danielgamboa10@gmail.com>
2021-06-11 20:32:03 -0500

    Add reverse 32-bit int

--- leetcode.py
+++ leetcode.py
@@ -336,3 +336,21 @@ class Solution:
             ans.append(0)
         
         return ans
+
+# **************************************************************************** #
+# *********** Exercise ******************************************************* #
+# **************************************************************************** #
+# Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x == 0:
+            return 0
+        
+        sign = int(x/abs(x))
+        
+        rev_num = int(str(abs(x))[::-1])*sign
+        
+        if rev_num > 2**31 - 1 or rev_num < -2**31:
+            return 0
+        else:
+            return rev_num
