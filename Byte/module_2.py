099dd047d31b07e2d59093f6643856aee813b2ac
Daniel Gamboa <danielgamboa10@gmail.com>
2021-06-21 13:31:09 -0500

    Add problem definition, input, ouputs (i.e. understand the problem) for module 2 day 1

--- Byte/module_2.py
+++ Byte/module_2.py
@@ -11,39 +11,75 @@
 
 # Exercise 1: Maximum Sum Subarray
 # Link: (https://leetcode.com/problems/maximum-subarray/)
-# Assumptions: 
+# Assumptions: all items are integers, can be positive / negative, no empty subarray 
 # Examples walk through ✅
-# Custom examples (edges): 
-# Function signature: 
+# Custom examples (edges): [7,-8,9] => 9
+# Function signature: def maxSubArray(nums: List[int]) -> int:
+# IN: array of integers / OUT: integer with max sum
+def maxSubArray(nums):
+  print(nums)
 
 
 # Exercise 2: Number of Islands
 # Link: (https://leetcode.com/problems/number-of-islands/)
-# Assumptions: 
+# Assumptions: grid edges are surrounded by water, 1s are land, 0s are water, 2D binary grid, not connected unless adjacent hz or vt, numbers are strings
 # Examples walk through ✅
-# Custom examples (edges): 
-# Function signature: 
+# Custom examples (edges):
+  # Input: grid = [
+  #   ["1","0","0","0","0"],
+  #   ["0","1","0","0","0"],
+  #   ["0","0","1","0","0"],
+  #   ["0","0","0","1","0"]
+  # ]
+  # Output: 4
+  # Input: grid = [
+  #   ["1","1","1","0","1"],
+  #   ["1","1","1","0","0"],
+  #   ["1","0","1","0","1"],
+  #   ["1","1","1","0","0"]
+  # ]
+  # Output: 3
+# Function signature: def numIslands(grid: List[List[str]]) -> int:
+# IN: matrix (i.e. list with lists are items with strings as items) / OUT: integer with islands count
+def numIslands(grid):
+  print(grid)
 
 
 # Exercise 3: Climbing Stairs
 # Link: (https://leetcode.com/problems/climbing-stairs/)
-# Assumptions: 
+# Assumptions: it takes n individual steps to the top, you can only climb by 1 or 2 steps, max 45 steps
 # Examples walk through ✅
-# Custom examples (edges): 
-# Function signature: 
+# Custom examples (edges):
+  # n = 4 => 5 (1 + 1 + 1 + 1, 2 + 1 + 1, 1 + 2 + 1, 1 + 1 + 2, 2 + 2)
+  # n = 5 => 8 (1 + 1 + 1 + 1 + 1, 2 + 1 + 1 + 1, 1 + 2 + 1 + 1, 1 + 1 + 2 + 1, 1 + 1 + 1 + 2, 2 + 2 + 1, 1 + 2 + 2, 2 + 1 + 2)
+  # Exhibits fibonacci sequence
+# Function signature: def climbStairs(n: int) -> int:
+# IN: number of steps integer / OUT: number of combinations integer
+def climbStairs(n):
+  print(n)
 
 
 # Exercise 4: Merge Intervals
 # Link: (https://leetcode.com/problems/merge-intervals/)
-# Assumptions: 
+# Assumptions: intervals are inclusive, contain two integers, 0 and positive integers only, start can equal end
 # Examples walk through ✅
 # Custom examples (edges): 
-# Function signature: 
+  # intervals = [[0,1],[2,3],[4,5],[5,10]] => [[0,1],[2,3],[4,10]]
+  # intervals = [[0,0],[0,0],[1,1],[1,5]] => [[0,0],[1,5]]
+# Function signature: def merge(intervals: List[List[int]]) -> List[List[int]]:
+# IN: intervals list (i.e. list of lists with two ints each) / OUT: consolidated list of lists
+def merge(intervals):
+  print(intervals)
 
 
 # Exercise 5: Max Binary Tree Depth
 # Link: (https://leetcode.com/problems/maximum-depth-of-binary-tree/)
-# Assumptions: 
+# Assumptions: root of binary tree has value, left, and right pointers; value is 0 and positive integers, root counts as 1 depth
 # Examples walk through ✅
 # Custom examples (edges): 
-# Function signature: 
+  # root = [3,9,20,null,null,15,7] => 3
+  # root = [1,3,5,null,2,15,7,6,null] => 4
+# Function signature: def maxDepth(root: TreeNode) -> int:
+# IN: binary tree root / OUT: max depth of the binary tree
+def maxDepth(root):
+  print(root)
