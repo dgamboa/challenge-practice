a7a965eaa48cea83483739de13bf4a15cd92d5bb
Daniel Gamboa <danielgamboa10@gmail.com>
2021-06-21 12:55:13 -0500

    Create module 2 workbook file

---
+++ Byte/module_2.py
@@ -0,0 +1,49 @@
+# Notes:
+# 1. Understand the problem [3-5 minutes]
+# 2. Find a brute force solution [5 minutes]
+# 3. Optimize your solution [15 minutes]
+# 4. Code your solution [15 minutes]
+# 5. Test your solution [5 minutes]
+
+# Day 1 ********************************************************************** #
+# Focus: understand the problem ********************************************** #
+# **************************************************************************** #
+
+# Exercise 1: Maximum Sum Subarray
+# Link: (https://leetcode.com/problems/maximum-subarray/)
+# Assumptions: 
+# Examples walk through ✅
+# Custom examples (edges): 
+# Function signature: 
+
+
+# Exercise 2: Number of Islands
+# Link: (https://leetcode.com/problems/number-of-islands/)
+# Assumptions: 
+# Examples walk through ✅
+# Custom examples (edges): 
+# Function signature: 
+
+
+# Exercise 3: Climbing Stairs
+# Link: (https://leetcode.com/problems/climbing-stairs/)
+# Assumptions: 
+# Examples walk through ✅
+# Custom examples (edges): 
+# Function signature: 
+
+
+# Exercise 4: Merge Intervals
+# Link: (https://leetcode.com/problems/merge-intervals/)
+# Assumptions: 
+# Examples walk through ✅
+# Custom examples (edges): 
+# Function signature: 
+
+
+# Exercise 5: Max Binary Tree Depth
+# Link: (https://leetcode.com/problems/maximum-depth-of-binary-tree/)
+# Assumptions: 
+# Examples walk through ✅
+# Custom examples (edges): 
+# Function signature: 
