4f3cddf5a96537a55841bdf543f64967d10d9c2e
Daniel Gamboa <danielgamboa10@gmail.com>
2021-07-05 20:37:42 -0500

    Add return true or false depending on whether intervals overlap

--- Educative/mergeIntervals.py
+++ Educative/mergeIntervals.py
@@ -28,3 +28,26 @@ def merge(intervals):
   merged.append(Interval(start, end))
 
   return merged
+
+
+# Given a set of intervals, find out if any two intervals overlap.
+
+# Time complexity O(n log n):
+def determineOverlap(intervals):
+  if len(intervals) < 2:
+    return false
+  
+  intervals.sort(key = lambda x: x.start)
+
+  start = intervals[0].start
+  end = intervals[0].end
+  
+  for i in range(1, len(intervals)):
+    interval = intervals[i]
+    if interval.start <= end:
+      return true
+    else:
+      start = interval.start
+      end = interval.end
+  
+  return false
